fd95fdc265ee64224db870ce53de89ec397add9d
Jim Faulkner <jhf1203@gmail.com>
2020-09-12 17:11:22 -0400

    created db & installed packages

---
+++ schema.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE employee-tracker;
+USE employee-tracker;
+
+CREATE TABLE department (
+  id INT AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE role (
+  id INT AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(9,2) NOT NULL,
+  department_id varchar(60) NOT NULL,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE employee (
+  id INT AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT 
+  PRIMARY KEY(id)
+);
+
